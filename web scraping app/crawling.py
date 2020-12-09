5929bc023212dfc24b783aa56f2cae41af4c1fbf
abdeljalil bensoudane <abdeljalilbensoudane97@gmail.com>
2020-12-09 23:48:05 +0100

    automating tasks capturing errors

--- web scraping app/crawling.py
+++
@@ -1,3 +0,0 @@
-from main import main
-
-main('https://www.amazon.com/')
--- web scraping app/get_links.py
+++
@@ -1,5 +0,0 @@
-import requests
-page = request
-def get_links(parsed_source,page):
-    links = []
-    for element in page.fin
